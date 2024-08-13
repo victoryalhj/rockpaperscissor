0616773c0fe4177211a7dc525fb57e96efc7968b
victoryalhj <victoryalhj@gmail.com>
2024-08-13 10:37:40 +0100

    Add box color and result

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Box from "./component/Box"
 // 4.The computer's choice is determined randomly
 // 5.Result of 3 & 4, you can recognize who is winner and loser 
 // 6.The border color of boxes changes according to the result (Winner-Green, Loser-Red, Tie-Black)
+// 컴퓨터결과+결과에 따른 테두리 바꾸기
 const choice = {
   rock:{
     name:"Rock",
@@ -23,18 +24,46 @@ const choice = {
   }
 }
 function App() {
-  const [userSelect, setUserSelect] = useState(null)
-
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect,setComputerSelect] = useState(null);
+  const [result,setResult]=useState("");
   const play=(userChoice)=>{
-    setUserSelect(choice[userChoice])
-  
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice],computerChoice));
   };
+  const judgement = (user,computer) => {
+    console.log("user", user, "computer", computer);
+
+    // user === computer tie
+    // user === rock, computer === "scissors" user win
+    // user === rock, computer === "paper" user lose
+    // user === scissor, computer === "paper" user win
+    // user === scissor, computer === "rock" user lose
+    // user === paper, computer === "rock" user win
+    // user === paper, computer === "scissors" user lose
+
+    if (user.name === computer.name ) {
+      return "tie"
+    }else if (user.name === "Rock") return computer.name === "Scissors"?"win":"lose"
+   else if(user.name === "Scissors")return computer.name === "Paper"?"win":"lose"
+   else if (user.name === "Paper")return computer.name === "Rock"?"win":"lose"
+  };
+
+  const randomChoice=()=>{
+    let itemArray = Object.keys(choice);
+    console.log("itemArray",itemArray);
+    let randomItem = Math.floor(Math.random()*itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  }
 
   return (
     <div>
       <div className ="main">
-        <Box title="You" choice={choice.rock} item={userSelect}/>
-        {/* <Box title="Computer" /> */}
+        <Box title="You" choice={choice.rock} item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>Scissors</button>
